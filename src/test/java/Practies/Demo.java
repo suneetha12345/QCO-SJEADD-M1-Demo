7a4fb93341f0d9b4da9178195beabb1586ee6ccd
Admin <Admin@DESKTOP-P450A6A>
2023-05-03 17:20:32 +0530

    commit and push

--- src/test/java/Practies/Demo.java
+++ src/test/java/Practies/Demo.java
@@ -3,7 +3,7 @@ package Practies;
 public class Demo {
 	public static void main(String[] args) {
 		
-		System.out.println("this is my first commit");
+		System.out.println("this is my commit");
 	}
 
 }
